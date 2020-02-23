79d85c33d53c47d71ff04c7689074aadf64cc394
BoatZako <boat_zako@hotmail.com>
2020-02-23 13:32:23 +0700

    Add debugger log

--- src/auth/route.js
+++ src/auth/route.js
@@ -8,19 +8,26 @@ module.exports = (passport) => {
   router.get('/facebook/signin', passport.authenticate('facebook'));
   router.get('/facebook/callback', function (req, res, next) {
     passport.authenticate('facebook', function (err, user, info) {
+      console.log('===================== INFO')
+      console.log(info)
       if (err) {
+        console.log('===================== ERROR')
+        console.log(err)
         return res.send(err);
       }
       if (!user) {
+        console.log('===================== USER')
         return res.redirect('/login');
       }
       req.logIn(user, function (err) {
         if (err) {
+          console.log('===================== LOGIN')
           return res.send(err);
         }
         return res.redirect('/');
       })
     })(req, res, next)
   });
+  
   return router
 }
