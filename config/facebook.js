e866137a24a11a42f6bc7d5998ddd2998b6b195e
BoatZako <boat_zako@hotmail.com>
2020-03-01 18:15:11 +0700

    debug

--- config/facebook.js
+++ config/facebook.js
@@ -1,4 +1,4 @@
 module.exports = {
-  clientID: process.env.FACEBOOK_CLIENT_ID || '2525400517780269',
-  clientSecret: process.env.FACEBOOK_CLIENT_SECRET || 'ddf38984e4b356195259fd05de850f78',
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
 }
