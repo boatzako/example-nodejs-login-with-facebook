a784023b0f2743679b941305353425b3ad3e7fc6
BoatZako <boat_zako@hotmail.com>
2020-03-01 17:24:20 +0700

    debug

--- src/auth/facebook.js
+++ src/auth/facebook.js
@@ -2,6 +2,10 @@ const Strategy = require('passport-facebook').Strategy
 const fbConfig = require('../../config/facebook')
 const serverConfig = require('../../config/server')
 
+console.log('=============== FACEBOOK =================');
+console.log(fbConfig.clientID);
+console.log(fbConfig.clientSecret);
+
 module.exports = new Strategy({
   clientID: fbConfig.clientID,
   clientSecret: fbConfig.clientSecret,
